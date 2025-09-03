5175593acb780938f6ad07b87a299615e5ac6af0
shivani211205 <shivanipatil8830@gmail.com>
2025-09-04 00:21:10 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Game {
+    public int number;
+    public int inputNumber;
+    public int noOfGuesses = 0;
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
+    }
+    Game() {
+        Random rand=new Random();
+        this.number=rand.nextInt(100);
+    }
+    void takeUserInput(){
+         System.out.println("Guess the number");
+        Scanner sc = new Scanner(System.in);
+        inputNumber =sc.nextInt();
+
+
+    }
+    boolean isCorrectNumber(){
+        noOfGuesses++;
+        if (inputNumber==number){
+            System.out.format("Yes you Guessed it right , it was %d\nYou guessed it in %d attempts  ",number,noOfGuesses);
+            return true;
+        }
+        else if(inputNumber<number){
+            System.out.println("Too Low...");
+        }
+        else if(inputNumber>number){
+            System.out.println("Too High...");
+        }
+        return false;
+    }
+}
+public class Number{
+    public static void main(String args[]){
+        Game g = new Game();
+        boolean b = false;
+        while (!b) {
+            
+        g.takeUserInput();
+        b = g.isCorrectNumber();
+        }
+    }
+}
